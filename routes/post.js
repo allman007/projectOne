686de0229f07ecd2141f4f1429cc17e90d922d91
allman007 <nwanjiuche@gmail.com>
2019-12-13 16:18:13 +0100

    class project

---
+++ routes/post.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const route = express.Router()
+const Post = require('../models/post')
+
+//post content
+route.post('/post', (req, res)=>{
+  const {title, content} = req.body
+  Post.create({title, content})
+  .then(post=>res.json(post))
+  .catch(err=>{
+    console.log(err);
+    res.json(err)
+  })
+})
+
+//fetch content
+route.get('/post', (req, res)=>{
+  Post.find()
+  .then(post=>res.json(post))
+  .catch(err=>res.json(err))
+})
+
+//fetch a content
+route.get('/post/:id', (req, res)=>{
+  Post.find({_id:req.params.id})
+  .then(post=>res.json(post))
+  .catch(err=>res.json(err))
+})
+
+//delete a post
+route.delete('/post', (req, res)=>{
+  Post.findOneAndDelete({_id:req.body.id})
+  .then(post=>res.json(post))
+  .catch(err=>res.json(err))
+})
+
+//update a post
+route.put('/post', (req, res)=>{
+  Post.findOneAndUpdate({_id:req.body.id}, {$set:{
+    title: req.body.title
+  }}, {upsert:false})
+  .then(post=>res.json(post))
+  .catch(err=>{
+    console.log(err);
+    res.json(err)
+  })
+})
+
+module.exports = route
